ba11413b0ddc38176b6f9b07f34cf653a7c6dac4
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-04-28 16:29:10 +0100

    Get product infos based on its bar code.

---
+++ getProduitByBarcode.php
@@ -0,0 +1,37 @@
+<?php
+require('connect.php');
+
+$response = array();
+if(!empty($_POST['barcode'])){
+	
+	$barcode = $_POST['barcode'];
+	$result = mysqli_query($con,"SELECT * from c_produit WHERE CodeBar =$barcode") or die(mysql_error());
+	 if(mysqli_num_rows($result) > 0){
+		 $response["success"] = 1;
+		$response["produit"] = array();
+		while ($row = mysqli_fetch_array($result)) {
+		
+			$Facts = array();
+			$Facts["idp"] = $row[0];
+			$Facts["libelle"] = $row[1];
+			$Facts["prixHT"] = $row[2];
+			$Facts["prixTTC"] = $row[3];
+			$Facts["codeBar"] = $row[4];
+			$Facts["photo"] = $row[5];
+			$Facts["qte"] = $row[6];
+			$Facts["local"] = $row[7];
+			array_push($response["produit"], $Facts);
+		
+			}
+	  }
+	  else{
+		$response["success"] = 0;
+	  }
+
+}else{
+	$response["erreur"] = "aucun produit trouvé!";
+	$response["success"] = 0;
+}
+  echo json_encode($response);
+  mysqli_close($con);
+?>
--- login.php
+++ login.php
@@ -3,40 +3,39 @@ require('connect.php');
 
 $response = array();
 
-if(!empty($_POST['Username']) && !empty($_POST['Password']) ){
-$username =$_POST['Username'];
-$password =$_POST['Password']; 
-$pwd=md5($password);
+if (!empty($_POST['Username']) && !empty($_POST['Password'])) {
+    $username = $_POST['Username'];
+    $password = $_POST['Password'];
+    $pwd = md5($password);
 
-	$result = mysqli_query($con,"SELECT * FROM c_utilisateur WHERE UserName='$username' AND Password='$pwd'") or die(mysql_error());
-     if(mysqli_num_rows($result) > 0){
-		$response["success"] = 1;
-		$response["user"] = array();
-		while ($row = mysqli_fetch_array($result)) {
-		
-			$Facts = array();
-			$Facts["iduser"] = $row[0];
-			$Facts["nom"] = $row[1];
-			$Facts["prenom"] = $row[2];
-			$Facts["type"] = $row[3];
-			$Facts["dateCreation"] = $row[4];
-			$Facts["dateExpiration"] = $row[5];
-			$Facts["Usename"] = $row[6];
-			$Facts["Password"] = $row[7];
-			$Facts["SocieteID"] = $row[8];
-			
-			array_push($response["user"], $Facts);
-		
-			}
-	  }
-	  else{
-		$response["success"] = 0;
-	  }
+    $result = mysqli_query($con, "SELECT * FROM c_utilisateur WHERE UserName='$username' AND Password='$pwd'") or die(mysql_error());
+    if (mysqli_num_rows($result) > 0) {
+        $response["success"] = 1;
+        $response["user"] = array();
+        while ($row = mysqli_fetch_array($result)) {
 
-}else{
-	$response["erreur"] = "Login or password invalid !";
-	$response["success"] = 0;
+            $Facts = array();
+            $Facts["iduser"] = $row[0];
+            $Facts["nom"] = $row[1];
+            $Facts["prenom"] = $row[2];
+            $Facts["type"] = $row[3];
+            $Facts["dateCreation"] = $row[4];
+            $Facts["dateExpiration"] = $row[5];
+            $Facts["Usename"] = $row[6];
+            $Facts["Password"] = $row[7];
+            $Facts["SocieteID"] = $row[8];
+
+            array_push($response["user"], $Facts);
+
+        }
+    } else {
+        $response["success"] = 0;
+    }
+
+} else {
+    $response["erreur"] = "Login or password invalid !";
+    $response["success"] = 0;
 }
-  echo json_encode($response);
-  mysqli_close($con);
+echo json_encode($response);
+mysqli_close($con);
 ?>
