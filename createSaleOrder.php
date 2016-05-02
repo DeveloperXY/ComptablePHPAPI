a457db4a41e3b855e85ee2ca8a60637a8628eed1
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-02 18:34:31 +0100

    createSaleOrder.php: Save the sale order ID using the 'FA + order_id' formula.

--- createSaleOrder.php
+++ createSaleOrder.php
@@ -14,25 +14,33 @@ if (isset($_POST['data'])) {
         $total += (floatval($product->priceTTC));
     }
 
-    $sql = "INSERT INTO c_commandevente VALUES (NULL, NOW(), 1, 'test', $total)";
+    $sql = "INSERT INTO c_commandevente VALUES (NULL, NOW(), 0, '', $total)";
     if (mysqli_query($con, $sql)) {
         $order_id = $con->insert_id;
-        $response["success"] = 1;
 
-        foreach ($obj as $product) {
-            $product_id = $product->productID;
-            $product_quantity = $product->quantity;
-            $product_ht = 0;
-            $product_ttc = 0;
-            $total = $product->priceTTC;
+        $facture_id = "FA" .$order_id;
+        $sql = "UPDATE c_commandevente SET IDFacture = '$facture_id' WHERE ID = $order_id";
+        if (mysqli_query($con, $sql)) {
+            $response["success"] = 1;
 
-            $sql = "INSERT INTO c_produit_has_commandevente VALUES ($product_id, $order_id, $product_quantity, $product_ht, $product_ttc, $total)";
-            if (mysqli_query($con, $sql)) {
-                // Empty for now
+            foreach ($obj as $product) {
+                $product_id = $product->productID;
+                $product_quantity = $product->quantity;
+                $product_ht = 0;
+                $product_ttc = $product->priceTTC;
+                $total = 0;
+
+                $sql = "INSERT INTO c_produit_has_commandevente VALUES ($product_id, $order_id, $product_quantity, $product_ht, $product_ttc, $total)";
+                if (mysqli_query($con, $sql)) {
+                    // Empty for now
+                }
+                else
+                    $response["success"] = -1; // -1: for debugging, different error code
             }
-            else
-                $response["success"] = -1; // -1: for debugging, different error code
         }
+        else
+//            $response["success"] = -2; // -2: for debugging, different error code
+            echo mysqli_error($con);
     }
     else
         $response["success"] = 0;
