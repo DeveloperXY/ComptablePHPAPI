2cd3a9902e97e49b636ed90474275796193d9c74
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-05 12:36:14 +0100

    Code refactor

--- getSaleOrder.php
+++ getSaleOrder.php
@@ -3,28 +3,25 @@ require('connect.php');
 
 $response = array();
 
-	$result = mysqli_query($con,"SELECT * from c_commandevente ORDER BY ID DESC") or die(mysql_error());
-	 if(mysqli_num_rows($result) > 0){
-		 $response["success"] = 1;
-		$response["sales"] = array();
-		while ($row = mysqli_fetch_array($result)) {
-		
-			$Facts = array();
-			$Facts["id"] = $row[0];
-			$Facts["date"] = $row[1];
-			$Facts["facture"] = $row[2];
-			$Facts["factureID"] = $row[3];
-			$Facts["total"] = $row[4];
-			
-			array_push($response["sales"], $Facts);
-		
-			}
-	  }
-	  else{
-		$response["success"] = 0;
-	  }
+$result = mysqli_query($con, "SELECT * from c_commandevente ORDER BY ID DESC") or die(mysql_error());
+if (mysqli_num_rows($result) > 0) {
+    $response["success"] = 1;
+    $response["orders"] = array();
+    while ($row = mysqli_fetch_array($result)) {
 
+        $Facts = array();
+        $Facts["id"] = $row[0];
+        $Facts["date"] = $row[1];
+        $Facts["facture"] = $row[2];
+        $Facts["facture"] = $row[3];
+        $Facts["total"] = $row[4];
 
-  echo json_encode($response);
-  mysqli_close($con);
+        array_push($response["orders"], $Facts);
+    }
+} else {
+    $response["success"] = 0;
+}
+
+echo json_encode($response);
+mysqli_close($con);
 ?>
--- getStock.php
+++ getStock.php
@@ -3,29 +3,29 @@ require('connect.php');
 
 $response = array();
 
-	$result = mysqli_query($con,"SELECT * from c_produit ORDER BY ID DESC") or die(mysql_error());
-	 if(mysqli_num_rows($result) > 0){
-		$response["products"] = array();
-		while ($row = mysqli_fetch_array($result)) {
-		
-			$Facts = array();
-			$Facts["idp"] = $row[0];
-			$Facts["libelle"] = $row[1];
-			$Facts["prixHT"] = $row[2];
-			$Facts["prixTTC"] = $row[3];
-			$Facts["codeBar"] = $row[4];
-			$Facts["photo"] = $row[5];
-			$Facts["qte"] = $row[6];
-			$Facts["local"] = $row[7];
-			array_push($response["products"], $Facts);
-		
-			}
-	  }
-	  else{
-		$response["success"] = 0;
-	  }
+$result = mysqli_query($con, "SELECT * from c_produit ORDER BY ID DESC") or die(mysql_error());
+if (mysqli_num_rows($result) > 0) {
+    $response["success"] = 1;
+    $response["products"] = array();
+    while ($row = mysqli_fetch_array($result)) {
+
+        $Facts = array();
+        $Facts["idp"] = $row[0];
+        $Facts["libelle"] = $row[1];
+        $Facts["prixHT"] = $row[2];
+        $Facts["prixTTC"] = $row[3];
+        $Facts["codeBar"] = $row[4];
+        $Facts["photo"] = $row[5];
+        $Facts["qte"] = $row[6];
+        $Facts["local"] = $row[7];
+        array_push($response["products"], $Facts);
+
+    }
+} else {
+    $response["success"] = 0;
+}
 
 
-  echo json_encode($response);
-  mysqli_close($con);
+echo json_encode($response);
+mysqli_close($con);
 ?>
