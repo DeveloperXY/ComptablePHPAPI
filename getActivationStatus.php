01e1ab54e08ddb60ecc30aef3498fc9aa842260e
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-11 12:43:34 +0100

    getActivationStatus.php: check if a serial is activated.

--- getActivationStatus.php
+++ getActivationStatus.php
@@ -3,21 +3,23 @@ require('connect.php');
 
 $response = array();
 
-$result = mysqli_query($con, "SELECT * from c_activation") or die(mysql_error());
-if (mysqli_num_rows($result) > 0) {
-    $response["success"] = 1;
-    $response["activation"] = array();
-    while ($row = mysqli_fetch_array($result)) {
-        $Facts = array();
-        $Facts["id"] = $row[0];
-        $Facts["code"] = $row[1];
-        $Facts["status"] = $row[2];
-        $Facts["companyID"] = $row[3];
-
-        array_push($response["activation"], $Facts);
+if(!empty($_POST['serial'])){
+    $code = $_POST['serial'];
+    
+    $result = mysqli_query($con, "SELECT * from c_activation WHERE code LIKE '$code'") or die(mysql_error());
+    if (mysqli_num_rows($result) > 0) {
+        $response["success"] = 1;
+        while ($row = mysqli_fetch_array($result)) {
+            $status = $row[2];
+            $response["activationStatus"] = $status == "oui" ? 1 : 0;
+        }
+    } else {
+        $response["success"] = 0;
     }
-} else {
-    $response["success"] = 0;
+
+}else{
+    $response["error"] = "You need to provide a serial parameter.";
+    $response["success"] = -1;
 }
 
 
