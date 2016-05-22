b2afdf7f56719d4b0a1d09dd0605f58b13c4cad8
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-22 09:22:02 +0100

    Add getChargeByLocaleID.php

---
+++ getChargeByLocaleID.php
@@ -0,0 +1,27 @@
+<?php
+require('connect.php');
+
+$response = array();
+
+if (isset($_POST["localeID"])) {
+    $locale_id = $_POST["localeID"];
+    $result = mysqli_query($con, "SELECT * from charge WHERE locale_id = $locale_id ORDER BY date DESC") or die(mysql_error());
+    if (mysqli_num_rows($result) > 0) {
+        $response["charge"] = array();
+        while ($row = mysqli_fetch_array($result)) {
+            $Facts = array();
+            $Facts["idcharge"] = $row[0];
+            $Facts["description"] = $row[1];
+            $Facts["prix"] = $row[2];
+            $Facts["date"] = $row[3];
+            $Facts["local"] = $row[4];
+            array_push($response["charge"], $Facts);
+        }
+    } else {
+        $response["success"] = 0;
+    }
+}
+
+echo json_encode($response);
+mysqli_close($con);
+?>
