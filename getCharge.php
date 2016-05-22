3ac3244e1692517fb6ec9de14fa5538e3fca4f6c
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-22 09:17:37 +0100

    Code refactor

--- getCharge.php
+++ getCharge.php
@@ -3,26 +3,22 @@ require('connect.php');
 
 $response = array();
 
-	$result = mysqli_query($con,"SELECT * from charge ORDER BY date DESC") or die(mysql_error());
-	 if(mysqli_num_rows($result) > 0){
-		$response["charge"] = array();
-		while ($row = mysqli_fetch_array($result)) {
-		
-			$Facts = array();
-			$Facts["idcharge"] = $row[0];
-			$Facts["description"] = $row[1];
-			$Facts["prix"] = $row[2];
-			$Facts["date"] = $row[3];
-			$Facts["local"] = $row[4];
-			array_push($response["charge"], $Facts);
-		
-			}
-	  }
-	  else{
-		$response["success"] = 0;
-	  }
+$result = mysqli_query($con, "SELECT * from charge ORDER BY date DESC") or die(mysql_error());
+if (mysqli_num_rows($result) > 0) {
+    $response["charge"] = array();
+    while ($row = mysqli_fetch_array($result)) {
+        $Facts = array();
+        $Facts["idcharge"] = $row[0];
+        $Facts["description"] = $row[1];
+        $Facts["prix"] = $row[2];
+        $Facts["date"] = $row[3];
+        $Facts["local"] = $row[4];
+        array_push($response["charge"], $Facts);
+    }
+} else {
+    $response["success"] = 0;
+}
 
-
-  echo json_encode($response);
-  mysqli_close($con);
+echo json_encode($response);
+mysqli_close($con);
 ?>
--- getStock.php
+++ getStock.php
@@ -3,11 +3,11 @@ require('connect.php');
 
 $response = array();
 
-if (isset($_POST["localeID"])) {
-    $id = $_POST["localeID"];
-    $result = mysqli_query($con, "SELECT * from c_produit WHERE locale_id = $id ORDER BY ID DESC") or die(mysql_error());
+if (isset($_GET["localeID"])) {
+    $id = $_GET["localeID"];
+    $result = mysqli_query($con, "SELECT * from c_produit WHERE locale_id = $id ORDER BY ID") or die(mysql_error());
     $response["success"] = 1;
-    $response["produit"] = array();
+    $response["products"] = array();
 
     if (mysqli_num_rows($result) > 0) {
         while ($row = mysqli_fetch_array($result)) {
@@ -20,7 +20,7 @@ if (isset($_POST["localeID"])) {
             $Facts["photo"] = $row[5];
             $Facts["qte"] = $row[6];
             $Facts["local"] = $row[7];
-            array_push($response["produit"], $Facts);
+            array_push($response["products"], $Facts);
         }
     }
 }
