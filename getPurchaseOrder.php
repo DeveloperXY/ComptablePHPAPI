cc2766eef43eebbbda4954f103b9942c751c2bc0
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-27 18:12:55 +0100

    Retrieve purchase & sale order by locale ID.

--- getPurchaseOrder.php
+++ getPurchaseOrder.php
@@ -3,21 +3,24 @@ require('connect.php');
 
 $response = array();
 
-$result = mysqli_query($con, "SELECT * from c_commandeachat ORDER BY ID DESC") or die(mysql_error());
+if (isset($_GET["localeID"])) {
+    $id = $_GET["localeID"];
+    $result = mysqli_query($con, "SELECT * from c_commandeachat WHERE idlocale = $id ORDER BY ID DESC") or die(mysql_error());
 
-$response["success"] = 1;
-$response["orders"] = array();
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_array($result)) {
+    $response["success"] = 1;
+    $response["orders"] = array();
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
 
-        $Facts = array();
-        $Facts["id"] = $row[0];
-        $Facts["date"] = $row[1];
-        $Facts["facture"] = $row[2];
-        $Facts["factureID"] = $row[3];
-        $Facts["total"] = $row[4];
+            $Facts = array();
+            $Facts["id"] = $row[0];
+            $Facts["date"] = $row[1];
+            $Facts["facture"] = $row[2];
+            $Facts["factureID"] = $row[3];
+            $Facts["total"] = $row[4];
 
-        array_push($response["orders"], $Facts);
+            array_push($response["orders"], $Facts);
+        }
     }
 }
 
