6bd0571de65eaa0238f814dd9445982936df62d8
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-27 19:17:53 +0100

    Retrieve suppliers by company ID.

--- getFournisseur.php
+++ getFournisseur.php
@@ -3,25 +3,29 @@ require('connect.php');
 
 $response = array();
 
-$result = mysqli_query($con, "SELECT * from c_fournisseur ORDER BY ID DESC") or die(mysql_error());
+if (isset($_POST["companyID"])) {
+    $id = $_POST["companyID"];
+    $result = mysqli_query($con, "SELECT * from c_fournisseur WHERE idsociete = $id ORDER BY ID DESC") or die(mysql_error());
 
-$response["success"] = 1;
-$response["fournisseur"] = array();
-if (mysqli_num_rows($result) > 0) {
-    while ($row = mysqli_fetch_array($result)) {
-        $Facts = array();
-        $Facts["idfournisseur"] = $row[0];
-        $Facts["nom"] = $row[1];
-        $Facts["adresse"] = $row[2];
-        $Facts["tel"] = $row[3];
-        $Facts["fix"] = $row[4];
-        $Facts["fax"] = $row[5];
-        $Facts["email"] = $row[6];
+    $response["success"] = 1;
+    $response["fournisseur"] = array();
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $Facts = array();
+            $Facts["idfournisseur"] = $row[0];
+            $Facts["nom"] = $row[1];
+            $Facts["adresse"] = $row[2];
+            $Facts["tel"] = $row[3];
+            $Facts["fix"] = $row[4];
+            $Facts["fax"] = $row[5];
+            $Facts["email"] = $row[6];
 
-        array_push($response["fournisseur"], $Facts);
+            array_push($response["fournisseur"], $Facts);
+        }
     }
 }
-
+else
+    $response["success"] = 0;
 
 echo json_encode($response);
 mysqli_close($con);
