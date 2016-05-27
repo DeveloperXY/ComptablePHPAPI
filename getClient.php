4fb6ecfabfbc4d645ccd7b3baf34f520c1a0a567
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-27 19:32:08 +0100

    Retrieve clients by company ID.

--- getClient.php
+++ getClient.php
@@ -3,26 +3,26 @@ require('connect.php');
 
 $response = array();
 
-$result = mysqli_query($con, "SELECT * from c_client ORDER BY ID DESC") or die(mysql_error());
-if (mysqli_num_rows($result) > 0) {
+if (isset($_POST["companyID"])) {
+    $id = $_POST["companyID"];
+    $result = mysqli_query($con, "SELECT * from c_client WHERE idsociete = $id ORDER BY ID DESC") or die(mysql_error());
     $response["success"] = 1;
     $response["client"] = array();
-    while ($row = mysqli_fetch_array($result)) {
+    if (mysqli_num_rows($result) > 0) {
+        while ($row = mysqli_fetch_array($result)) {
+            $Facts = array();
+            $Facts["idclient"] = $row[0];
+            $Facts["nom"] = $row[1];
+            $Facts["tel"] = $row[2];
+            $Facts["adresse"] = $row[3];
+            $Facts["email"] = $row[4];
 
-        $Facts = array();
-        $Facts["idclient"] = $row[0];
-        $Facts["nom"] = $row[1];
-        $Facts["tel"] = $row[2];
-        $Facts["adresse"] = $row[3];
-        $Facts["email"] = $row[4];
-
-        array_push($response["client"], $Facts);
-
+            array_push($response["client"], $Facts);
+        }
     }
-} else {
-    $response["success"] = 0;
 }
-
+else
+    $response["success"] = 0;
 
 echo json_encode($response);
 mysqli_close($con);
