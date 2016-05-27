dcfe6dcec9d4aed537a04eacff21dd2a5d805592
Mohammed Aouf ZOUAG <mohammedaoufzouag@outlook.com>
2016-05-27 17:47:14 +0100

    Bug fix

--- getServerTime.php
+++ getServerTime.php
@@ -5,9 +5,12 @@ $response = array();
 
 $result = mysqli_query($con, "SELECT DATE_FORMAT(NOW(), '%Y-%m-%d %H:%i:%s')") or die(mysql_error());
 if (mysqli_num_rows($result) > 0) {
-    $response["date"] = mysqli_fetch_array($result)[0];
+    $row = mysqli_fetch_array($result);
+    $response["date"] = $row[0];
+    $response["success"] = 1;
 } else {
     $response["date"] = "error";
+    $response["success"] = 0;
 }
 
 echo json_encode($response);
--- login.php
+++ login.php
@@ -6,12 +6,15 @@ $response = array();
 if (!empty($_POST['Username']) && !empty($_POST['Password'])) {
     $username = $_POST['Username'];
     $password = $_POST['Password'];
+	$idsociete= $_POST['companyID'];
     $pwd = md5($password);
 
-    $result = mysqli_query($con, "SELECT * FROM c_utilisateur WHERE UserName='$username' AND Password='$pwd'") or die(mysql_error());
+    $result = mysqli_query($con, "SELECT * FROM c_utilisateur WHERE UserName='$username' AND Password='$pwd' AND Société_ID=$idsociete ") or die(mysql_error());
     if (mysqli_num_rows($result) > 0) {
         $response["success"] = 1;
         $response["user"] = array();
+		$response["locals"] = array();
+		$idlocal=0;
         while ($row = mysqli_fetch_array($result)) {
 
             $Facts = array();
@@ -24,10 +27,28 @@ if (!empty($_POST['Username']) && !empty($_POST['Password'])) {
             $Facts["Username"] = $row[6];
             $Facts["Password"] = $row[7];
             $Facts["SocieteID"] = $row[8];
-
+			$idlocal=$row[9];
+			
             array_push($response["user"], $Facts);
 
         }
+		
+			if($row[3]=="Patron"){
+				$req = mysqli_query($con, "SELECT * FROM `c_locale` WHERE Société_ID=$idsociete") or die(mysql_error());
+			}else{
+				$req = mysqli_query($con, "SELECT * FROM `c_locale` WHERE ID=$idlocal") or die(mysql_error());
+			}
+			 while ($r = mysqli_fetch_array($req)) {
+					$local=array();
+					$local["Adresse"] = $r['Adresse'];
+					$local["Ville"] = $r['Ville'];
+					$local["Pays"] = $r['Pays'];
+					$local["Tel"] = $r['Tel'];
+					
+					array_push($response["locals"], $local);
+			 }
+
+			
     } else {
         $response["success"] = 0;
     }
